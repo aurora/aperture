4067d40c057ab05a8bdd19e677332c1226b6184f
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-10-26 14:56:57 +0000

    added file

---
+++ aperture/src/java/org/semanticdesktop/aperture/rdf/Vocabulary.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2005 Aduna and Deutsches Forschungszentrum für Künstliche Intelligenz DFKI GmbH.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.rdf;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
+
+/**
+ * Vocabulary defines a comprehensive list of URIs to be used in communication with various Aperture
+ * components.
+ */
+public class Vocabulary {
+   
+    public static final String NS = "http://aperture.semanticdesktop.org/ontology/data#";
+    
+    public static final String FULL_TEXT = "full-text";
+
+    public static final String CREATOR = "creator";
+    
+    static {
+        ValueFactory factory = new ValueFactoryImpl();
+        FULL_TEXT_URI = factory.createURI(FULL_TEXT);
+        CREATOR_URI = factory.createURI(CREATOR);
+    }
+    
+    public static final URI FULL_TEXT_URI;
+    
+    public static final URI CREATOR_URI;
+
+    private Vocabulary() {
+    	// prevents instantiation of this class
+    }
+}
