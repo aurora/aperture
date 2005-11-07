ee5f49e4793e3eb8262ba376df6cb0992e0278f7
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-07 12:08:37 +0000

    added extra PDF test cases

--- aperture/src/test/org/semanticdesktop/aperture/extractor/pdf/PdfExtractorTest.java
+++ aperture/src/test/org/semanticdesktop/aperture/extractor/pdf/PdfExtractorTest.java
@@ -23,6 +23,10 @@ public class PdfExtractorTest extends ExtractorTestBase {
     
     private static final String PDF_CREATOR_DOC = DOCS_PATH + "pdf-word-2000-pdfcreator-0.8.0.pdf";
     
+    private static final String PDF_MAKER_DOC = DOCS_PATH + "pdf-word-2000-pdfmaker-7.0.pdf";
+    
+    private static final String PDF_WRITER_DOC = DOCS_PATH + "pdf-word-2000-pdfwriter-7.0.pdf";
+    
     private RDFContainerSesame getStatements(String resourceName) throws ExtractorException, IOException {
         // apply the extractor
         ExtractorFactory factory = new PdfExtractorFactory();
@@ -33,23 +37,51 @@ public class PdfExtractorTest extends ExtractorTestBase {
     public void testOpenOffice2Writer() throws ExtractorException, IOException {
         // note: document has no date
         RDFContainerSesame container = getStatements(OPEN_OFFICE_2_DOC);
+        
         checkStatement(Vocabulary.GENERATOR_URI, "OpenOffice", container);
+        
         checkOmnipresentStatements(container);
     }
     
     public void testOpenOffice1Writer() throws ExtractorException, IOException {
         // note: document has no date
         RDFContainerSesame container = getStatements(OPEN_OFFICE_1_DOC);
+        
         checkStatement(Vocabulary.GENERATOR_URI, "OpenOffice", container);
+        
         checkOmnipresentStatements(container);
     }
     
     public void testPDFCreator() throws ExtractorException, IOException {
         RDFContainerSesame container = getStatements(PDF_CREATOR_DOC);
+        
         checkStatement(Vocabulary.GENERATOR_URI, "PDFCreator", container);
         checkStatement(Vocabulary.GENERATOR_URI, "Ghostscript", container);
         checkStatement(Vocabulary.DATE_URI, "2005", container);
+        
         checkOmnipresentStatements(container);
+    }
+    
+    public void testPDFMaker() throws ExtractorException, IOException {
+        RDFContainerSesame container = getStatements(PDF_MAKER_DOC);
+        
+        checkStatement(Vocabulary.GENERATOR_URI, "PDFMaker", container);
+        checkStatement(Vocabulary.GENERATOR_URI, "Distiller", container);
+        checkStatement(Vocabulary.DATE_URI, "2005", container);
+        
+        checkOmnipresentStatements(container);
+    }
+    
+    public void testPDFWriter() throws ExtractorException, IOException {
+        RDFContainerSesame container = getStatements(PDF_WRITER_DOC);
+        
+        checkStatement(Vocabulary.TITLE_URI, "Microsoft Word", container);
+        checkStatement(Vocabulary.CREATOR_URI, "Christiaan Fluit", container);
+        checkStatement(Vocabulary.GENERATOR_URI, "PScript5.dll", container);
+        checkStatement(Vocabulary.GENERATOR_URI, "Distiller", container);
+        checkStatement(Vocabulary.DATE_URI, "2005", container);
+        checkStatement(Vocabulary.CREATION_DATE_URI, "2005", container);
+        checkStatement(Vocabulary.PAGE_COUNT_URI, "1", container);
     }
     
     private void checkOmnipresentStatements(RDFContainerSesame container) {
