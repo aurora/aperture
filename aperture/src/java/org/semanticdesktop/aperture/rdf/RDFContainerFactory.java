e6410028a67d17987e20f14f449543e329b4343e
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-14 14:50:12 +0000

    added a factory for RDFContainers

---
+++ aperture/src/java/org/semanticdesktop/aperture/rdf/RDFContainerFactory.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2005 Aduna and Deutsches Forschungszentrum für Künstliche Intelligenz DFKI GmbH.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.rdf;
+
+import org.openrdf.model.URI;
+
+/**
+ * RDFContainerFactories create new RDFContainers on demand.
+ */
+public interface RDFContainerFactory {
+
+    /**
+     * Create a new empty RDFContainer describing the specified ID.
+     * 
+     * @param id The URI of the described resource.
+     * @return A new empty RDFContainer that will describe the specified ID.
+     */
+    public RDFContainer newInstance(String id);
+
+    /**
+     * Create a new empty RDFContainer describing the specified ID.
+     * 
+     * @param id The URI of the described resource.
+     * @return A new empty RDFContainer that will describe the specified ID.
+     */
+    public RDFContainer newInstance(URI id);
+}
