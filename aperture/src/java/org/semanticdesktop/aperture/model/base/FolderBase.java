2a92c78ed8f046d02b505b67f29f4a0e34e7b1ea
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-15 13:46:03 +0000

    added class

---
+++ aperture/src/java/org/semanticdesktop/aperture/model/base/FolderBase.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2005 Aduna and Deutsches Forschungszentrum für Künstliche Intelligenz DFKI GmbH.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.model.base;
+
+import org.openrdf.model.URI;
+import org.semanticdesktop.aperture.model.DataSource;
+import org.semanticdesktop.aperture.model.Folder;
+import org.semanticdesktop.aperture.rdf.RDFContainer;
+
+/**
+ * A trivial default implementation of Folder. 
+ */
+public class FolderBase extends DataObjectBase implements Folder {
+
+    public FolderBase(URI id, DataSource dataSource, RDFContainer metadata) {
+        super(id, dataSource, metadata);
+    }
+}
