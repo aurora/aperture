c189cb37d7776581ef6d71829970408680212fd2
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-17 09:27:07 +0000

    added a test case related to efficient incremental crawling

--- aperture/src/test/org/semanticdesktop/aperture/access/file/TestFileAccessor.java
+++ aperture/src/test/org/semanticdesktop/aperture/access/file/TestFileAccessor.java
@@ -9,15 +9,18 @@ package org.semanticdesktop.aperture.access.file;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.HashMap;
 
 import org.openrdf.model.URI;
 import org.openrdf.model.impl.URIImpl;
 import org.semanticdesktop.aperture.ApertureTestBase;
+import org.semanticdesktop.aperture.access.AccessData;
 import org.semanticdesktop.aperture.access.BinaryObject;
 import org.semanticdesktop.aperture.access.DataObject;
 import org.semanticdesktop.aperture.access.Folder;
 import org.semanticdesktop.aperture.access.UrlNotFoundException;
 import org.semanticdesktop.aperture.access.Vocabulary;
+import org.semanticdesktop.aperture.access.base.AccessDataBase;
 import org.semanticdesktop.aperture.rdf.sesame.SesameRDFContainer;
 import org.semanticdesktop.aperture.rdf.sesame.SesameRDFContainerFactory;
 import org.semanticdesktop.aperture.util.FileUtil;
@@ -26,55 +29,69 @@ import org.semanticdesktop.aperture.util.IOUtil;
 public class TestFileAccessor extends ApertureTestBase {
 
     private static final String TMP_SUBDIR = "TestFileAccessor.tmpDir";
-    
+
     private File tmpDir;
-    
+
     private File tmpFile;
-    
+
     private FileAccessor fileAccessor;
-    
+
     public void setUp() throws IOException {
         // create a temporary folder containing a temporary file
         // unfortunately there is no File.createTempDir
         tmpDir = new File(System.getProperty("java.io.tmpdir"), TMP_SUBDIR).getCanonicalFile();
         tmpDir.mkdir();
-        
+
         tmpFile = File.createTempFile("file-", ".txt", tmpDir);
         IOUtil.writeString("test file", tmpFile);
-        
+
         // setup a FileAccessor
         FileAccessorFactory factory = new FileAccessorFactory();
         factory.setContainerFactory(new SesameRDFContainerFactory());
         fileAccessor = (FileAccessor) factory.get();
     }
-    
+
     public void tearDown() {
         // delete the temporary folder
         FileUtil.deltree(tmpDir);
     }
-    
+
     public void testFileAccess() throws UrlNotFoundException, MalformedURLException, IOException {
         // create the data object
         DataObject dataObject = fileAccessor.get(tmpFile.toURI().toString(), null, null, null);
         assertNotNull(dataObject);
         assertTrue(dataObject instanceof BinaryObject);
-        
+
         // check its metadata
-        SesameRDFContainer metadata = (SesameRDFContainer) dataObject.getMetadata();  
+        SesameRDFContainer metadata = (SesameRDFContainer) dataObject.getMetadata();
         checkStatement(Vocabulary.NAME_URI, "file-", metadata);
-        
+
         URI parentURI = new URIImpl(tmpDir.toURI().toString());
         checkStatement(Vocabulary.PART_OF_URI, parentURI, metadata);
     }
-    
+
     public void testFolderAccess() throws UrlNotFoundException, MalformedURLException, IOException {
         // create the data object
         DataObject dataObject = fileAccessor.get(tmpDir.toURI().toString(), null, null, null);
         assertNotNull(dataObject);
         assertTrue(dataObject instanceof Folder);
-        
+
         // check its metadata
-        SesameRDFContainer metadata = (SesameRDFContainer) dataObject.getMetadata();  
+        SesameRDFContainer metadata = (SesameRDFContainer) dataObject.getMetadata();
         checkStatement(Vocabulary.NAME_URI, "TestFileAccessor", metadata);
+    }
+
+    public void testNonModifiedFile() throws UrlNotFoundException, IOException {
+        // create a fake AccessData that holds the last modified date of tmpFile
+        AccessData accessData = new AccessDataBase();
+        String id = "file:/does.not.matter";
+        accessData.put(id, AccessData.DATE_KEY, String.valueOf(tmpFile.lastModified()));
+
+        // check that the FileAccessor returns null when we try to fetch a DataObject while passing it
+        // this AccessData
+        HashMap params = new HashMap();
+        params.put(FileAccessor.FILE_KEY, tmpFile);
+        DataObject object = fileAccessor.get(id, null, accessData, params);
+        assertNull(object);
     }
 }
