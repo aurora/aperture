4f6d5a4990f588eb743937d3ef687368ed8232e5
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-17 11:14:43 +0000

    renamed getDateTimeFormat to getISO8601DateFormat, added comments

--- aperture/src/java/org/semanticdesktop/aperture/util/RDFUtil.java
+++ aperture/src/java/org/semanticdesktop/aperture/util/RDFUtil.java
@@ -10,30 +10,38 @@ import java.util.Date;
  */
 public class RDFUtil {
 
-    private static DateFormat dateTimeFormat = null;
+    private static DateFormat dateFormat = null;
 
     /**
-     * format the given date in a good dateTime format: ISO 8601, using the T seperator and the - and :
-     * seperators accordingly. example: 2003-01-22T17:00:00
+     * Format the given date in a good dateTime format: ISO-8601, using the T separator and the - and :
+     * seperators accordingly. Example: 2003-01-22T17:00:00.
      * 
-     * @param date a date
-     * @return a formatted string.
+     * @param date A date instance.
+     * @return A String containing the date in ISO-8601 format.
      */
     public static String dateTime2String(Date date) {
-        return getDateTimeFormat().format(date);
+        return getISO8601DateFormat().format(date);
     }
 
-    public static Date string2DateTime(String string) throws ParseException {
-        return getDateTimeFormat().parse(string);
-    }
-    
     /**
-     * the datetimeformat that conforms to ISO 8601, which is used in XSD-datetime.
+     * Parses the given string as a Date using the same date format as dateTime2String.
+     * 
+     * @param string A String in ISO-8601 format.
+     * @return A Date instance with a timestamp obtained from the specified String.
+     * @see #dateTime2String(Date)
      */
-    public static DateFormat getDateTimeFormat() {
-        if (dateTimeFormat == null) {
-            dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    public static Date string2DateTime(String string) throws ParseException {
+        return getISO8601DateFormat().parse(string);
+    }
+
+    /**
+     * Returns a statically shared DateFormat that uses the ISO-8601 format, which is used by
+     * XSD-DATETIME.
+     */
+    public static DateFormat getISO8601DateFormat() {
+        if (dateFormat == null) {
+            dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         }
-        return dateTimeFormat;
+        return dateFormat;
     }
 }
