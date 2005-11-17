ac3e7a14a55874dd8cb310eef0df07d582e555b4
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-17 11:07:39 +0000

    added string2DateTime method, reformatted code

--- aperture/src/java/org/semanticdesktop/aperture/util/RDFUtil.java
+++ aperture/src/java/org/semanticdesktop/aperture/util/RDFUtil.java
@@ -1,29 +1,32 @@
 package org.semanticdesktop.aperture.util;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * provides helper methods to handle RDF
- * 
- * 
- * @author Sauermann
- * $Id$
+ * Provides helper methods to handle RDF statements.
  */
 public class RDFUtil {
-    
+
     private static DateFormat dateTimeFormat = null;
+
     /**
-     * format the given date in a good dateTime format:
-     * ISO 8601, using the T seperator and the - and : seperators accordingly.
-     * example: 2003-01-22T17:00:00
+     * format the given date in a good dateTime format: ISO 8601, using the T seperator and the - and :
+     * seperators accordingly. example: 2003-01-22T17:00:00
+     * 
      * @param date a date
      * @return a formatted string.
      */
     public static String dateTime2String(Date date) {
-        return getDateTimeFormat().format(date);        
+        return getDateTimeFormat().format(date);
     }
+
+    public static Date string2DateTime(String string) throws ParseException {
+        return getDateTimeFormat().parse(string);
+    }
+    
     /**
      * the datetimeformat that conforms to ISO 8601, which is used in XSD-datetime.
      */
@@ -34,11 +37,3 @@ public class RDFUtil {
         return dateTimeFormat;
     }
 }
-
-
-/*
- * $Log$
- * Revision 1.1  2005/10/26 14:08:59  leo_sauermann
- * added the sesame-model and began with RDFContainer
- *
- */
