e46c37a55780c1a654d0361e0c3e760e4ffddca5
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-20 20:48:36 +0000

    added class

---
+++ aperture/src/java/org/semanticdesktop/aperture/datasource/filesystem/FileSystemDataSource.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2005 Aduna.
+ * All rights reserved.
+ * 
+ * Licensed under the Open Software License version 3.0.
+ */
+package org.semanticdesktop.aperture.datasource.filesystem;
+
+import org.semanticdesktop.aperture.datasource.base.DataSourceBase;
+
+/**
+ * A FileSystemDataSource defines a collection of Files residing on a local or shared drive.
+ */
+public class FileSystemDataSource extends DataSourceBase {
+
+}
