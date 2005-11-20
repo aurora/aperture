1fb2cdba7b843977ba5a7099fd29eae821718504
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-11-20 19:28:49 +0000

    added base DataSource implementation

---
+++ aperture/src/java/org/semanticdesktop/aperture/datasource/base/DataSourceBase.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2005 Aduna.
+ * All rights reserved.
+ * 
+ * Licensed under the Open Software License version 3.0.
+ */
+package org.semanticdesktop.aperture.datasource.base;
+
+import org.openrdf.model.URI;
+import org.semanticdesktop.aperture.datasource.DataSource;
+import org.semanticdesktop.aperture.datasource.Vocabulary;
+import org.semanticdesktop.aperture.rdf.RDFContainer;
+
+/**
+ * A trivial default implementation of the DataSource interface.
+ */
+public class DataSourceBase implements DataSource {
+
+    // Note: the utility get methods operating on the RDFContainer interpret invalid data as no
+    // configuration, i.e. exceptions result in null return values. This is in line with the general
+    // spirit of RDF that it should be ossible to make any arbitrary statement, only some statements
+    // cannot be interpreted automatically.
+
+    private URI id;
+
+    private String name;
+
+    private RDFContainer configuration;
+
+    public URI getID() {
+        return id;
+    }
+
+    public void setID(URI id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RDFContainer getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(RDFContainer configuration) {
+        // check whether the described URI matches our ID
+        URI describedURI = configuration.getDescribedUri();
+        if (describedURI == null) {
+            throw new IllegalArgumentException("RDFContainer has no described URI");
+        }
+        else if (!describedURI.equals(id)) {
+            throw new IllegalArgumentException("described URI and ID do not match, ID = " + id
+                    + ", described URI = " + describedURI);
+        }
+
+        // set the configuration
+        this.configuration = configuration;
+    }
+    
+    public void setRootUrl(String url) {
+        RDFContainer configuration = getConfiguration();
+        configuration.put(Vocabulary.ROOT_URL, url);
+    }
+    
+    public String getRootUrl() {
+        return getConfiguration().getString(Vocabulary.ROOT_URL);
+    }
+    
+    public void setMaximumDepth(int maximumDepth) {
+        getConfiguration().put(Vocabulary.MAXIMUM_DEPTH, maximumDepth);
+    }
+
+    public int getMaximumDepth() {
+        Integer value = getConfiguration().getInteger(Vocabulary.MAXIMUM_DEPTH);
+        if (value == null) {
+            // interpret as no depth restrictions
+            return -1;
+        }
+        else {
+            return value.intValue();
+        }
+    }
+
+    // fixme: add methods for adding and removing patterns
+    // perhaps use RDFReactor right away for this?
+}
