fd83583dadf92567ebf8b6b5e6b08ea976dc6058
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2005-12-23 11:11:52 +0000

    made implementation similar to how ExampleImapCrawler is constructed

--- aperture/src/examples/org/semanticdesktop/aperture/examples/ExampleFileCrawler.java
+++ aperture/src/examples/org/semanticdesktop/aperture/examples/ExampleFileCrawler.java
@@ -42,7 +42,7 @@ import org.semanticdesktop.aperture.extractor.ExtractorFactory;
 import org.semanticdesktop.aperture.extractor.ExtractorRegistry;
 import org.semanticdesktop.aperture.extractor.impl.DefaultExtractorRegistry;
 import org.semanticdesktop.aperture.mime.identifier.MimeTypeIdentifier;
-import org.semanticdesktop.aperture.mime.identifier.magic.MagicMimeTypeIdentifierFactory;
+import org.semanticdesktop.aperture.mime.identifier.magic.MagicMimeTypeIdentifier;
 import org.semanticdesktop.aperture.rdf.RDFContainer;
 import org.semanticdesktop.aperture.rdf.sesame.SesameRDFContainer;
 import org.semanticdesktop.aperture.util.IOUtil;
@@ -52,44 +52,77 @@ import org.semanticdesktop.aperture.util.IOUtil;
  */
 public class ExampleFileCrawler {
 
+    private static final Logger LOGGER = Logger.getLogger(ExampleFileCrawler.class.getName());
+
     public static final String IDENTIFY_MIME_TYPE_OPTION = "-identifyMimeType";
 
     public static final String EXTRACT_CONTENTS_OPTION = "-extractContents";
 
     public static final String VERBOSE_OPTION = "-verbose";
 
-    private static final Logger LOGGER = Logger.getLogger(ExampleFileCrawler.class.getName());
-    
-    private static File rootFile = null;
+    private File rootFile;
 
-    private static File repositoryFile = null;
+    private File repositoryFile;
 
-    private static boolean identifyMimeType = false;
+    private boolean identifyingMimeType = false;
 
-    private static boolean extractContents = false;
+    private boolean extractingContents = false;
 
-    private static MimeTypeIdentifier mimeTypeIdentifier;
+    private boolean verbose = false;
 
-    private static ExtractorRegistry extractorRegistry;
-    
-    private static boolean verbose = false;
+    public boolean isExtractingContents() {
+        return extractingContents;
+    }
 
-    public static void main(String[] args) {
-        // parse the command line arguments
-        parse(args);
+    public boolean isIdentifyingMimeType() {
+        return identifyingMimeType;
+    }
 
-        // create components for processing file contents
-        if (identifyMimeType) {
-            mimeTypeIdentifier = new MagicMimeTypeIdentifierFactory().get();
-            if (extractContents) {
-                extractorRegistry = new DefaultExtractorRegistry();
-            }
+    public File getRepositoryFile() {
+        return repositoryFile;
+    }
+
+    public File getRootFile() {
+        return rootFile;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setExtractingContents(boolean extractingContents) {
+        this.extractingContents = extractingContents;
+    }
+
+    public void setIdentifyingMimeType(boolean identifyingMimeType) {
+        this.identifyingMimeType = identifyingMimeType;
+    }
+
+    public void setRepositoryFile(File repositoryFile) {
+        this.repositoryFile = repositoryFile;
+    }
+
+    public void setRootFile(File rootFile) {
+        this.rootFile = rootFile;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    public void crawl() {
+        if (rootFile == null) {
+            throw new IllegalArgumentException("root file cannot be null");
+        }
+        if (repositoryFile == null) {
+            throw new IllegalArgumentException("repository file cannot be null");
         }
 
         // create a FileSystemDataSource
         FileSystemDataSource source = new FileSystemDataSource();
         URI sourceID = new URIImpl("source:testSource");
         source.setID(sourceID);
+        source.setName("Example file system source");
 
         // configure the directory tree to crawl
         SesameRDFContainer configuration = new SesameRDFContainer(sourceID);
@@ -106,38 +139,39 @@ public class ExampleFileCrawler {
         crawler.crawl();
     }
 
-    private static void parse(String[] args) {
-        // parse the command line arguments
+    public static void main(String[] args) {
+        // create a new ExampleFileCrawler instance
+        ExampleFileCrawler crawler = new ExampleFileCrawler();
+        
+        // parse the command line options
         for (int i = 0; i < args.length; i++) {
             String arg = args[i];
             if (IDENTIFY_MIME_TYPE_OPTION.equals(arg)) {
-                identifyMimeType = true;
+                crawler.setIdentifyingMimeType(true);
             }
             else if (EXTRACT_CONTENTS_OPTION.equals(arg)) {
-                extractContents = true;
+                crawler.setExtractingContents(true);
             }
             else if (VERBOSE_OPTION.equals(arg)) {
-                verbose = true;
+                crawler.setVerbose(true);
             }
             else if (arg.startsWith("-")) {
                 System.err.println("Unknown option: " + arg);
                 exitWithUsageMessage();
             }
-            else if (rootFile == null) {
-                rootFile = new File(arg);
+            else if (crawler.getRootFile() == null) {
+                crawler.setRootFile(new File(arg));
             }
-            else if (repositoryFile == null) {
-                repositoryFile = new File(arg);
+            else if (crawler.getRepositoryFile() == null) {
+                crawler.setRepositoryFile(new File(arg));
             }
             else {
                 exitWithUsageMessage();
             }
         }
 
-        // exit when no root dir or repository file was specified
-        if (rootFile == null || repositoryFile == null) {
-            exitWithUsageMessage();
-        }
+        // start crawling and exit afterwards
+        crawler.crawl();
     }
 
     private static void exitWithUsageMessage() {
@@ -145,14 +179,26 @@ public class ExampleFileCrawler {
                 + "] [" + EXTRACT_CONTENTS_OPTION + "] [" + VERBOSE_OPTION + "] rootDirectory repositoryFile");
         System.exit(-1);
     }
+    
+    private class SimpleCrawlerHandler implements CrawlerHandler, RDFContainerFactory {
 
-    private static class SimpleCrawlerHandler implements CrawlerHandler, RDFContainerFactory {
+        private MimeTypeIdentifier mimeTypeIdentifier;
+
+        private ExtractorRegistry extractorRegistry;
 
         private Repository repository;
 
         private int nrObjects;
 
         public SimpleCrawlerHandler() {
+            // create some identification and extraction components
+            if (identifyingMimeType) {
+                mimeTypeIdentifier = new MagicMimeTypeIdentifier();
+            }
+            if (extractingContents) {
+                extractorRegistry = new DefaultExtractorRegistry();
+            }
+
             // create a Repository
             repository = new Repository(new MemoryStore());
 
@@ -197,7 +243,6 @@ public class ExampleFileCrawler {
         }
 
         public void accessingObject(Crawler crawler, String url) {
-            nrObjects++;
             if (verbose) {
                 System.out.println("Processing file " + nrObjects + ": " + url + "...");
             }
@@ -212,78 +257,98 @@ public class ExampleFileCrawler {
             container.setContext(uri);
             return container;
         }
-        
+
         public void objectNew(Crawler dataCrawler, DataObject object) {
-            if (object instanceof FileDataObject) {
-                process((FileDataObject) object);
-            }
+            nrObjects++;
+
+            // Commit the changes to the repository. This needs to happen before processing of the
+            // FileDataObjects as that operation may one day query for certain statements (mimetypes
+            // etc).
             commit();
+
+            // process the contents on an InputStream, if available
+            if (object instanceof FileDataObject) {
+                try {
+                    process((FileDataObject) object);
+                    commit();
+                }
+                catch (IOException e) {
+                    LOGGER.log(Level.WARNING, "IOException while processing " + object.getID(), e);
+                }
+                catch (ExtractorException e) {
+                    LOGGER.log(Level.WARNING, "ExtractorException while processing " + object.getID(), e);
+                }
+            }
         }
 
         public void objectChanged(Crawler dataCrawler, DataObject object) {
             printUnexpectedEventWarning("changed");
-            commit();
         }
 
         public void objectNotModified(Crawler crawler, String url) {
             printUnexpectedEventWarning("unmodified");
-            commit();
         }
 
         public void objectRemoved(Crawler dataCrawler, String url) {
             printUnexpectedEventWarning("removed");
-            commit();
         }
 
-        private void process(FileDataObject object) {
-            if (!identifyMimeType) {
+        public void clearStarted(Crawler crawler) {
+            printUnexpectedEventWarning("clearStarted");
+        }
+
+        public void clearingObject(Crawler crawler, String url) {
+            printUnexpectedEventWarning("clearingObject");
+        }
+
+        public void clearFinished(Crawler crawler, ExitCode exitCode) {
+            printUnexpectedEventWarning("clear finished");
+        }
+
+        private void process(FileDataObject object) throws IOException, ExtractorException {
+            // we cannot do anything when MIME type identification is disabled
+            if (!identifyingMimeType) {
                 return;
             }
 
             URI id = object.getID();
 
-            try {
-                // Create a buffer around the object's stream large enough to be able to reset the stream
-                // after MIME type identification has taken place. Add some extra to the minimum array
-                // length required by the MimeTypeIdentifier for safety.
-                int minimumArrayLength = mimeTypeIdentifier.getMinArrayLength();
-                int bufferSize = Math.max(minimumArrayLength, 8192);
-                BufferedInputStream buffer = new BufferedInputStream(object.getContent(), bufferSize);
-                buffer.mark(minimumArrayLength + 10); // add some for safety
+            // Create a buffer around the object's stream large enough to be able to reset the stream
+            // after MIME type identification has taken place. Add some extra to the minimum array
+            // length required by the MimeTypeIdentifier for safety.
+            int minimumArrayLength = mimeTypeIdentifier.getMinArrayLength();
+            int bufferSize = Math.max(minimumArrayLength, 8192);
+            BufferedInputStream buffer = new BufferedInputStream(object.getContent(), bufferSize);
+            buffer.mark(minimumArrayLength + 10); // add some for safety
 
-                // apply the MimeTypeIdentifier
-                byte[] bytes = IOUtil.readBytes(buffer, minimumArrayLength);
-                String mimeType = mimeTypeIdentifier.identify(bytes, null, id);
+            // apply the MimeTypeIdentifier
+            byte[] bytes = IOUtil.readBytes(buffer, minimumArrayLength);
+            String mimeType = mimeTypeIdentifier.identify(bytes, null, id);
 
-                if (mimeType != null) {
-                    // add the mime type to the metadata
-                    RDFContainer metadata = object.getMetadata();
-                    metadata.put(Vocabulary.MIME_TYPE, mimeType);
+            if (mimeType != null) {
+                // add the mime type to the metadata
+                RDFContainer metadata = object.getMetadata();
+                metadata.put(Vocabulary.MIME_TYPE, mimeType);
 
-                    // apply an Extractor if available
-                    if (extractContents) {
-                        buffer.reset();
+                // apply an Extractor if available
+                if (extractingContents) {
+                    buffer.reset();
 
-                        Set extractors = extractorRegistry.get(mimeType);
-                        if (!extractors.isEmpty()) {
-                            ExtractorFactory factory = (ExtractorFactory) extractors.iterator().next();
-                            Extractor extractor = factory.get();
-                            extractor.extract(id, buffer, null, mimeType, metadata);
-                        }
+                    Set extractors = extractorRegistry.get(mimeType);
+                    if (!extractors.isEmpty()) {
+                        ExtractorFactory factory = (ExtractorFactory) extractors.iterator().next();
+                        Extractor extractor = factory.get();
+                        extractor.extract(id, buffer, null, mimeType, metadata);
                     }
                 }
-            }
-            catch (IOException e) {
-                LOGGER.log(Level.WARNING, "IOException while processing " + id, e);
-            }
-            catch (ExtractorException e) {
-                LOGGER.log(Level.WARNING, "ExtractorException while processing " + id, e);
             }
         }
 
         private void commit() {
             try {
-                repository.commit();
+                if (repository.isActive()) {
+                    repository.commit();
+                }
             }
             catch (SailUpdateException e) {
                 // don't continue when this happens
@@ -294,18 +359,6 @@ public class ExampleFileCrawler {
         private void printUnexpectedEventWarning(String event) {
             // as we don't keep track of access data in this example code, some events should never occur
             LOGGER.warning("encountered unexpected event (" + event + ") with non-incremental crawler");
-        }
-
-        public void clearStarted(Crawler crawler) {
-            // no-op
-        }
-
-        public void clearingObject(Crawler crawler, String url) {
-            // no-op
-        }
-
-        public void clearFinished(Crawler crawler, ExitCode exitCode) {
-            // no-op
         }
     }
 }
