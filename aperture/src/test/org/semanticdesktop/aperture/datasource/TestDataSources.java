e35302162e852359b0e353e45fb99008689b68d2
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-01-19 12:51:37 +0000

    added DomainBoundaries unit test

--- aperture/src/test/org/semanticdesktop/aperture/datasource/TestDataSources.java
+++ aperture/src/test/org/semanticdesktop/aperture/datasource/TestDataSources.java
@@ -7,6 +7,7 @@
 package org.semanticdesktop.aperture.datasource;
 
 import org.semanticdesktop.aperture.datasource.config.TestConfigurationUtil;
+import org.semanticdesktop.aperture.datasource.config.TestDomainBoundaries;
 import org.semanticdesktop.aperture.datasource.impl.TestDefaultDataSourceRegistry;
 
 import junit.framework.Test;
@@ -24,6 +25,7 @@ public class TestDataSources extends TestSuite {
     private TestDataSources() {
         super("data sources");
         addTest(new TestSuite(TestDefaultDataSourceRegistry.class));
+        addTest(new TestSuite(TestDomainBoundaries.class));
         addTest(new TestSuite(TestConfigurationUtil.class));
     }
 }
---
+++ aperture/src/test/org/semanticdesktop/aperture/datasource/config/TestDomainBoundaries.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2005 Aduna.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.datasource.config;
+
+import org.semanticdesktop.aperture.ApertureTestBase;
+
+public class TestDomainBoundaries extends ApertureTestBase {
+
+    private DomainBoundaries boundaries;
+    
+    public void setUp() {
+        boundaries = new DomainBoundaries();
+    }
+
+    public void tearDown() {
+        boundaries = null;
+    }
+    
+    public void testEmptyBoundaries() {
+        assertTrue(boundaries.inDomain("http://example.com"));
+    }
+    
+    public void testIncludePatterns() {
+        boundaries.addIncludePattern(new SubstringPattern(".java", new SubstringCondition.EndsWith()));
+        assertTrue(boundaries.inDomain("file:test.java"));
+        assertFalse(boundaries.inDomain("file:test.doc"));
+    }
+    
+    public void testExcludePatterns() {
+        boundaries.addExcludePattern(new SubstringPattern(".java", new SubstringCondition.EndsWith()));
+        assertFalse(boundaries.inDomain("file:test.java"));
+        assertTrue(boundaries.inDomain("file:test.doc"));        
+    }
+    
+    public void testBothPatterns() {
+        boundaries.addIncludePattern(new SubstringPattern(".java", new SubstringCondition.EndsWith()));
+        boundaries.addExcludePattern(new SubstringPattern("test", new SubstringCondition.Contains()));
+        assertFalse(boundaries.inDomain("file:test.doc"));
+        assertTrue(boundaries.inDomain("file:aperture.java"));
+        assertFalse(boundaries.inDomain("file:test.java"));
+        assertFalse(boundaries.inDomain("file:test/aperture.java"));
+    }
+}
