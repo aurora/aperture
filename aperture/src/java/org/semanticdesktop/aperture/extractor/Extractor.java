23c71ace35929ca4dab30ff385d5687626188da5
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-01-31 11:30:57 +0000

    refined Javadoc: remark that the InputStream is expected to use some sort of buffering, it's not the Extractor's responsibility

--- aperture/src/java/org/semanticdesktop/aperture/extractor/Extractor.java
+++ aperture/src/java/org/semanticdesktop/aperture/extractor/Extractor.java
@@ -13,24 +13,29 @@ import org.openrdf.model.URI;
 import org.semanticdesktop.aperture.rdf.RDFContainer;
 
 /**
- * Extractors extract information from binary streams such as document full-text, titles, authors and
- * other metadata that may be supported by the format. Extractors are typically specific for a single
- * MIME type or a number of closely related MIME types.
+ * Extractors extract information from binary streams such as document full-text, titles, authors and other
+ * metadata that may be supported by the format. Extractors are typically specific for a single MIME type or a
+ * number of closely related MIME types.
  */
 public interface Extractor {
 
 	/**
-	 * Extracts full-text and metadata from the specified binary stream and stores the extracted
-	 * information as RDF statements in the specified RDFContainer. The optionally specified Charset and
-	 * MIME type can be used to direct how the stream should be parsed.
+	 * Extracts full-text and metadata from the specified binary stream and stores the extracted information
+	 * as RDF statements in the specified RDFContainer. The optionally specified Charset and MIME type can be
+	 * used to direct how the stream should be parsed.
 	 * 
-	 * @param id the URI identifying the object (e.g. a file or web page) from which the stream was
-	 *            obtained. The generated statements should describe this URI.
+	 * <p>
+	 * The specified InputStream is expected to already use some kind of buffering so that the Extractors are
+	 * not required to internally buffer bytes to improve performance.
+	 * 
+	 * @param id the URI identifying the object (e.g. a file or web page) from which the stream was obtained.
+	 *            The generated statements should describe this URI.
 	 * @param stream the InputStream delivering the raw bytes.
 	 * @param charset the charset in which the inputstream is encoded (optional).
 	 * @param mimeType the MIME type of the passed stream (optional).
 	 * @param result the container in which this Extractor can put its created RDF statements.
 	 * @throws ExtractorException in case of any error during the extraction process.
 	 */
-	public void extract(URI id, InputStream stream, Charset charset, String mimeType, RDFContainer result) throws ExtractorException;
+	public void extract(URI id, InputStream stream, Charset charset,
+			String mimeType, RDFContainer result) throws ExtractorException;
 }
