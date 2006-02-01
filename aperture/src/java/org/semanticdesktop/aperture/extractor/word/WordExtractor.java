3a28835bf1d85d85005e457b2c2eab078b80bad0
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-02-01 11:23:57 +0000

    added initial version of WordExtractor. Still to do: metadata extraction, fallback in case POI throws exceptions, investigate inclusion of Apache POI/TextMining.org code

---
+++ aperture/src/java/org/semanticdesktop/aperture/extractor/word/WordExtractor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2005 Aduna.
+ * All rights reserved.
+ * 
+ * Licensed under the Open Software License version 3.0.
+ */
+package org.semanticdesktop.aperture.extractor.word;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+
+import org.apache.poi.hdf.extractor.WordDocument;
+import org.openrdf.model.URI;
+import org.semanticdesktop.aperture.accessor.Vocabulary;
+import org.semanticdesktop.aperture.extractor.Extractor;
+import org.semanticdesktop.aperture.extractor.ExtractorException;
+import org.semanticdesktop.aperture.rdf.RDFContainer;
+
+public class WordExtractor implements Extractor {
+
+	public void extract(URI id, InputStream stream, Charset charset, String mimeType, RDFContainer result)
+			throws ExtractorException {
+		try {
+			WordDocument document = new WordDocument(stream);
+			StringWriter writer = new StringWriter(64 * 1024);
+			document.writeAllText(writer);
+			String text = writer.toString().trim();
+			if (!text.equals("")) {
+				result.put(Vocabulary.FULL_TEXT, text);
+			}
+		}
+		catch (IOException e) {
+			throw new ExtractorException(e);
+		}
+	}
+}
