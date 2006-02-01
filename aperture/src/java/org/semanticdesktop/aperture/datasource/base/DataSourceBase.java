680d79739931c7a297312508f926a2e92ff1d520
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-02-01 10:57:27 +0000

    setConfiguration now also allows passing a null value

--- aperture/src/java/org/semanticdesktop/aperture/datasource/base/DataSourceBase.java
+++ aperture/src/java/org/semanticdesktop/aperture/datasource/base/DataSourceBase.java
@@ -15,49 +15,51 @@ import org.semanticdesktop.aperture.rdf.RDFContainer;
  */
 public abstract class DataSourceBase implements DataSource {
 
-    // Note: the utility get methods operating on the RDFContainer interpret invalid data as no
-    // configuration, i.e. exceptions result in null return values. This is in line with the general
-    // spirit of RDF that it should be possible to make any arbitrary statement, only some statements
-    // cannot be interpreted automatically.
+	// Note: the utility get methods operating on the RDFContainer interpret invalid data as no
+	// configuration, i.e. exceptions result in null return values. This is in line with the general
+	// spirit of RDF that it should be possible to make any arbitrary statement, only some statements
+	// cannot be interpreted automatically.
 
-    private URI id;
+	private URI id;
 
-    private String name;
+	private String name;
 
-    private RDFContainer configuration;
+	private RDFContainer configuration;
 
-    public URI getID() {
-        return id;
-    }
+	public URI getID() {
+		return id;
+	}
 
-    public void setID(URI id) {
-        this.id = id;
-    }
+	public void setID(URI id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public RDFContainer getConfiguration() {
-        return configuration;
-    }
+	public RDFContainer getConfiguration() {
+		return configuration;
+	}
 
-    public void setConfiguration(RDFContainer configuration) {
-        // check whether the described URI matches our ID
-        URI describedURI = configuration.getDescribedUri();
-        if (describedURI == null) {
-            throw new IllegalArgumentException("RDFContainer has no described URI");
-        }
-        else if (!describedURI.equals(id)) {
-            throw new IllegalArgumentException("described URI and ID do not match, ID = " + id
-                    + ", described URI = " + describedURI);
-        }
+	public void setConfiguration(RDFContainer configuration) {
+		// check whether the described URI matches our ID
+		if (configuration != null) {
+			URI describedURI = configuration.getDescribedUri();
+			if (describedURI == null) {
+				throw new IllegalArgumentException("RDFContainer has no described URI");
+			}
+			else if (!describedURI.equals(id)) {
+				throw new IllegalArgumentException("described URI and ID do not match, ID = " + id
+						+ ", described URI = " + describedURI);
+			}
+		}
 
-        // set the configuration
-        this.configuration = configuration;
-    }
+		// set the configuration
+		this.configuration = configuration;
+	}
 }
