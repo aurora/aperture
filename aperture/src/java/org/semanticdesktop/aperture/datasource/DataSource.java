757072811cc4076ba84881bba0305cce03566bf2
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-02-01 10:56:47 +0000

    added setConfiguration method

--- aperture/src/java/org/semanticdesktop/aperture/datasource/DataSource.java
+++ aperture/src/java/org/semanticdesktop/aperture/datasource/DataSource.java
@@ -10,53 +10,61 @@ import org.openrdf.model.URI;
 import org.semanticdesktop.aperture.rdf.RDFContainer;
 
 /**
- * A DataSource defines the characteristics of a source from which DataObjects can be extracted. A
- * Datasource contains all information necessary to realize these objects, such as paths, usernames,
- * passwords, etc.
+ * A DataSource defines the characteristics of a source from which DataObjects can be extracted. A Datasource
+ * contains all information necessary to realize these objects, such as paths, usernames, passwords, etc.
  */
 public interface DataSource {
 
-    /**
-     * Gets the id of this data source.
-     * 
-     * @return A URI identifier for the data source.
-     */
-    public URI getID();
+	/**
+	 * Gets the id of this data source.
+	 * 
+	 * @return A URI identifier for the data source.
+	 */
+	public URI getID();
 
-    /**
-     * Set the ID of this data source.
-     * 
-     * @param id The new ID of this DataSource.
-     */
-    public void setID(URI id);
+	/**
+	 * Set the ID of this data source.
+	 * 
+	 * @param id The new ID of this DataSource.
+	 */
+	public void setID(URI id);
 
-    /**
-     * Gets the name of this data source.
-     * 
-     * @return A descriptive name for the data source.
-     */
-    public String getName();
+	/**
+	 * Gets the name of this data source.
+	 * 
+	 * @return A descriptive name for the data source.
+	 */
+	public String getName();
 
-    /**
-     * Sets the name of this data source.
-     * 
-     * @param name A descriptive name for the data source.
-     */
-    public void setName(String name);
+	/**
+	 * Sets the name of this data source.
+	 * 
+	 * @param name A descriptive name for the data source.
+	 */
+	public void setName(String name);
 
-    /**
-     * Returns a URI that indicates the DataSource type in a platform- and programming
-     * language-independent way.
-     * 
-     * @return A URI indicating the DataSource type
-     */
-    public URI getType();
+	/**
+	 * Returns a URI that indicates the DataSource type in a platform- and programming language-independent
+	 * way.
+	 * 
+	 * @return A URI indicating the DataSource type
+	 */
+	public URI getType();
 
-    /**
-     * Gets the RDF model containing the configuration of this DataSource. The returned RDFContainer can
-     * be modified in order to update the configuration.
-     * 
-     * @return A mutable RDFContainer.
-     */
-    public RDFContainer getConfiguration();
+	/**
+	 * Gets the RDF model containing the configuration of this DataSource. The returned RDFContainer can be
+	 * modified in order to update the configuration.
+	 * 
+	 * @return A mutable RDFContainer, or 'null' when the configuration container has not been set yet.
+	 */
+	public RDFContainer getConfiguration();
+
+	/**
+	 * Sets the RDF model containing the configuration of this DataSource. The specified RDFContainer is
+	 * expected to be mutable.
+	 * 
+	 * @param configuration A mutable RDFContainer, or 'null' when the DataSource's configuration needs to be
+	 *            temporarily unset.
+	 */
+	public void setConfiguration(RDFContainer configuration);
 }
