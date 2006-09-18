6353c613b2031bc6b3ad23a2a82136c535a51ce7
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-09-18 08:48:33 +0000

    HttpURLConnections now get proper read and connect timeouts (was: infinity) so that non-responsive servers no longer ruin crawling

--- aperture/src/java/org/semanticdesktop/aperture/accessor/http/HttpAccessor.java
+++ aperture/src/java/org/semanticdesktop/aperture/accessor/http/HttpAccessor.java
@@ -36,248 +36,259 @@ import org.semanticdesktop.aperture.vocabulary.DATA;
  */
 public class HttpAccessor implements DataAccessor {
 
-    private static final Logger LOGGER = Logger.getLogger(HttpAccessor.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(HttpAccessor.class.getName());
 
-    /**
-     * Key used to store that a url is known. This is necessary because not every url has a date that can
-     * be stored and we still have to separate undated urls from known urls. This key has no
-     * corresponding value.
-     */
-    private static final String ACCESSED_KEY = "accessed";
+	/**
+	 * Key used to store that a url is known. This is necessary because not every url has a date that can be
+	 * stored and we still have to separate undated urls from known urls. This key has no corresponding value.
+	 */
+	private static final String ACCESSED_KEY = "accessed";
 
-    private static final String DATE_KEY = "date";
+	private static final String DATE_KEY = "date";
 
-    private static final int MAX_REDIRECTIONS = 20;
-    
-    private int connectTimeout = 20000;
-    
-    private int readTimeout = 20000;
+	private static final int MAX_REDIRECTIONS = 20;
 
-    public void setConnectTimeout(int timeout) {
-    	this.connectTimeout = timeout;
-    }
-    
-    public int getConnectTimeout() {
-    	return connectTimeout;
-    }
-    
-    public void setReadTimeout(int timeout) {
-    	this.readTimeout = timeout;
-    }
-    
-    public int getReadTimeout() {
-    	return readTimeout;
-    }
-    
-    public DataObject getDataObject(String url, DataSource source, Map params,
-            RDFContainerFactory containerFactory) throws UrlNotFoundException, IOException {
-        return get(url, source, null, params, containerFactory);
-    }
+	private int connectTimeout = 20000;
 
-    public DataObject getDataObjectIfModified(String url, DataSource source, AccessData accessData,
-            Map params, RDFContainerFactory containerFactory) throws UrlNotFoundException, IOException {
-        return get(url, source, accessData, params, containerFactory);
-    }
+	private int readTimeout = 20000;
 
-    private DataObject get(String urlString, DataSource source, AccessData accessData, Map params,
-            RDFContainerFactory containerFactory) throws UrlNotFoundException, IOException {
-        // keep a backup of the originally passed url
-        String originalUrlString = urlString;
+	public void setConnectTimeout(int timeout) {
+		this.connectTimeout = timeout;
+	}
 
-        HttpURLConnection connection;
-        int nrRedirections = 0;
+	public int getConnectTimeout() {
+		return connectTimeout;
+	}
 
-        // We're going to loop, accessing urls until we arrive at a url that is not redirected. The
-        // redirection is followed manually rather than automatically, which is HttpURLConnection's
-        // default behaviour, so that we know the actual url we arrive at.
-        while (true) {
-            // check if we haven't been redirected too often
-            if (nrRedirections > MAX_REDIRECTIONS) {
-                throw new IOException("too many redirections, max = " + MAX_REDIRECTIONS + ", url = "
-                        + originalUrlString);
-            }
+	public void setReadTimeout(int timeout) {
+		this.readTimeout = timeout;
+	}
 
-            // create an encoded URL instance
-            URL url = new URL(UriUtil.encodeUri(urlString));
+	public int getReadTimeout() {
+		return readTimeout;
+	}
 
-            // normalize the URL
-            url = UrlUtil.normalizeURL(url);
-            urlString = url.toExternalForm();
+	public DataObject getDataObject(String url, DataSource source, Map params,
+			RDFContainerFactory containerFactory) throws UrlNotFoundException, IOException {
+		return get(url, source, null, params, containerFactory);
+	}
 
-            // see if a date was registered for this url
-            // DO NOT USE ?:  -> will make nullpointer exception
-            Date ifModifiedSince;
-            if (accessData == null )
-            {
-            	ifModifiedSince = null;
-            }
-            else
-            	ifModifiedSince = getIfModifiedSince(urlString, accessData);
+	public DataObject getDataObjectIfModified(String url, DataSource source, AccessData accessData,
+			Map params, RDFContainerFactory containerFactory) throws UrlNotFoundException, IOException {
+		return get(url, source, accessData, params, containerFactory);
+	}
 
-            // set up a connection (a HttpAccessor always has HttpURLConnections, else it's a bug)
-            connection = createConnection(url, ifModifiedSince);
+	private DataObject get(String urlString, DataSource source, AccessData accessData, Map params,
+			RDFContainerFactory containerFactory) throws UrlNotFoundException, IOException {
+		// keep a backup of the originally passed url
+		String originalUrlString = urlString;
 
-            // send the request to the server
-            connection.connect();
+		HttpURLConnection connection = null;
+		int nrRedirections = 0;
 
-            // check for http-specific response codes
-            int responseCode = connection.getResponseCode();
+		// We're going to loop, accessing urls until we arrive at a url that is not redirected. The
+		// redirection is followed manually rather than automatically, which is HttpURLConnection's
+		// default behaviour, so that we know the actual url we arrive at.
+		while (true) {
+			// check if we haven't been redirected too often
+			if (nrRedirections > MAX_REDIRECTIONS) {
+				throw new IOException("too many redirections, max = " + MAX_REDIRECTIONS + ", url = "
+						+ originalUrlString);
+			}
 
-            if (isRedirected(responseCode)) {
-                // follow the redirected url
-                urlString = getRedirectedUrl(url, connection);
-                nrRedirections++;
-            }
-            else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
-                throw new UrlNotFoundException(urlString);
-            }
-            else if (responseCode == HttpURLConnection.HTTP_NOT_MODIFIED) {
-                // bail out ASAP
-                return null;
-            }
-            else if (responseCode != HttpURLConnection.HTTP_OK) {
-                // this is a communication error, quit with an exception
-                throw new IOException("Http connection error, response code = " + responseCode + ", url = "
-                        + url);
-            }
-            else {
-                // we're done
-                break;
-            }
-        }
+			// create an encoded URL instance
+			URL url = new URL(UriUtil.encodeUri(urlString));
 
-        // create the actual data object
-        DataObject result = createDataObject(urlString, source, connection, containerFactory);
+			// normalize the URL
+			url = UrlUtil.normalizeURL(url);
+			urlString = url.toExternalForm();
 
-        // register it in the access data
-        if (accessData != null) {
-            // make sure we always store something about this url, so that accessData.isKnownId will
-            // return true
-            accessData.put(urlString, ACCESSED_KEY, "");
+			// see if a date was registered for this url
+			// DO NOT USE ?: -> will make nullpointer exception
+			Date ifModifiedSince;
+			if (accessData == null) {
+				ifModifiedSince = null;
+			}
+			else
+				ifModifiedSince = getIfModifiedSince(urlString, accessData);
 
-            // also store this for the original url, so that crawlers can detect that they have accessed
-            // a redirected url before (it won't be considered as being a new object, every time it is
-            // accessed)
-            if (nrRedirections > 0) {
-                accessData.put(originalUrlString, ACCESSED_KEY, "");
-            }
+			try {
+				// set up a connection (a HttpAccessor always has HttpURLConnections, else it's a bug)
+				connection = createConnection(url, ifModifiedSince);
 
-            // store the date with which we can check in the next access whether the object was modified
-            long date = connection.getDate();
-            if (date != 0L) {
-                accessData.put(urlString, DATE_KEY, String.valueOf(date));
-            }
-        }
+				// send the request to the server
+				connection.connect();
+			}
+			catch (Exception e) {
+				// I've seen IllegalArgumentExceptions in the sun.net classes here because of some freaky URLs
+				// that did not generate MalformedUrlExceptions
+				if (e instanceof IOException) {
+					throw (IOException) e;
+				}
+				else {
+					throw new UrlNotFoundException(originalUrlString, "connection resulted in a "
+							+ e.getClass() + " exception");
+				}
+			}
 
-        return result;
-    }
+			// check for http-specific response codes
+			int responseCode = connection.getResponseCode();
 
-    private Date getIfModifiedSince(String urlString, AccessData accessData) {
-        if (accessData == null) {
-            return null;
-        }
-        else {
-            String value = accessData.get(urlString, DATE_KEY);
-            if (value == null) {
-                return null;
-            }
-            
-            try {
-                long l = Long.parseLong(value);
-                return new Date(l);
-            }
-            catch (NumberFormatException e) {
-                LOGGER.log(Level.WARNING, "invalid long: " + value, e);
-                return null;
-            }
-        }
-    }
+			if (isRedirected(responseCode)) {
+				// follow the redirected url
+				urlString = getRedirectedUrl(url, connection);
+				nrRedirections++;
+			}
+			else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+				throw new UrlNotFoundException(urlString);
+			}
+			else if (responseCode == HttpURLConnection.HTTP_NOT_MODIFIED) {
+				// bail out ASAP
+				return null;
+			}
+			else if (responseCode != HttpURLConnection.HTTP_OK) {
+				// this is a communication error, quit with an exception
+				throw new IOException("Http connection error, response code = " + responseCode + ", url = "
+						+ url);
+			}
+			else {
+				// we're done
+				break;
+			}
+		}
 
-    private HttpURLConnection createConnection(URL url, Date ifModifiedSince) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setConnectTimeout(connectTimeout);
-        connection.setReadTimeout(readTimeout);
-        HttpClientUtil.setAcceptGZIPEncoding(connection);
-        connection.setInstanceFollowRedirects(false);
-        if (ifModifiedSince != null) {
-            connection.setIfModifiedSince(ifModifiedSince.getTime());
-        }
+		// create the actual data object
+		DataObject result = createDataObject(urlString, source, connection, containerFactory);
 
-        return connection;
-    }
+		// register it in the access data
+		if (accessData != null) {
+			// make sure we always store something about this url, so that accessData.isKnownId will
+			// return true
+			accessData.put(urlString, ACCESSED_KEY, "");
 
-    private boolean isRedirected(int responseCode) {
-        return responseCode == HttpURLConnection.HTTP_MULT_CHOICE
-                || responseCode == HttpURLConnection.HTTP_MOVED_PERM
-                || responseCode == HttpURLConnection.HTTP_MOVED_TEMP
-                || responseCode == HttpURLConnection.HTTP_SEE_OTHER;
-    }
+			// also store this for the original url, so that crawlers can detect that they have accessed
+			// a redirected url before (it won't be considered as being a new object, every time it is
+			// accessed)
+			if (nrRedirections > 0) {
+				accessData.put(originalUrlString, ACCESSED_KEY, "");
+			}
 
-    private String getRedirectedUrl(URL url, HttpURLConnection connection) throws IOException {
-        String newLocation = connection.getHeaderField("Location");
-        if (newLocation == null) {
-            throw new IOException("missing redirection location");
-        }
-        else {
-            return new URL(url, newLocation).toString();
-        }
-    }
+			// store the date with which we can check in the next access whether the object was modified
+			long date = connection.getDate();
+			if (date != 0L) {
+				accessData.put(urlString, DATE_KEY, String.valueOf(date));
+			}
+		}
 
-    private DataObject createDataObject(String url, DataSource source, HttpURLConnection connection,
-            RDFContainerFactory containerFactory) throws IOException {
-        // create the resulting instance
-        URI uri = new URIImpl(url);
-        RDFContainer metadata = containerFactory.getRDFContainer(uri);
-        
-        InputStream stream = HttpClientUtil.getInputStream(connection);
-        if (!stream.markSupported()) {
-            stream = new BufferedInputStream(stream, 8192);
-        }
-        
-        DataObject object = new FileDataObjectBase(uri, source, metadata, stream);
+		return result;
+	}
 
-        // populate the metadata
-        String characterSet = null;
-        String mimeType = null;
+	private Date getIfModifiedSince(String urlString, AccessData accessData) {
+		if (accessData == null) {
+			return null;
+		}
+		else {
+			String value = accessData.get(urlString, DATE_KEY);
+			if (value == null) {
+				return null;
+			}
 
-        String contentType = connection.getContentType();
-        if (contentType != null) {
-            ContentType parsedType = new ContentType(contentType);
-            characterSet = parsedType.getCharset();
-            mimeType = parsedType.getMimeType();
-        }
+			try {
+				long l = Long.parseLong(value);
+				return new Date(l);
+			}
+			catch (NumberFormatException e) {
+				LOGGER.log(Level.WARNING, "invalid long: " + value, e);
+				return null;
+			}
+		}
+	}
 
-        if (characterSet == null) {
-            // character set defaults to ISO-8859-1 (Latin-1) for HTTP
-            characterSet = "ISO-8859-1";
-        }
+	private HttpURLConnection createConnection(URL url, Date ifModifiedSince) throws IOException {
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setConnectTimeout(connectTimeout);
+		connection.setReadTimeout(readTimeout);
+		HttpClientUtil.setAcceptGZIPEncoding(connection);
+		connection.setInstanceFollowRedirects(false);
+		if (ifModifiedSince != null) {
+			connection.setIfModifiedSince(ifModifiedSince.getTime());
+		}
 
-        metadata.add(DATA.characterSet, characterSet);
-        
-        if (mimeType != null) {
-            metadata.add(DATA.mimeType, mimeType);
-        }
+		return connection;
+	}
 
-        long contentLength = connection.getContentLength();
-        if (contentLength >= 0l) {
-            metadata.add(DATA.byteSize, contentLength);
-        }
-        
-        long retrieved = connection.getDate();
-        if (retrieved != 0L) {
-            metadata.add(DATA.retrievalDate, new Date(retrieved));
-        }
+	private boolean isRedirected(int responseCode) {
+		return responseCode == HttpURLConnection.HTTP_MULT_CHOICE
+				|| responseCode == HttpURLConnection.HTTP_MOVED_PERM
+				|| responseCode == HttpURLConnection.HTTP_MOVED_TEMP
+				|| responseCode == HttpURLConnection.HTTP_SEE_OTHER;
+	}
 
-        long lastModified = connection.getLastModified();
-        if (lastModified != 0L) {
-            metadata.add(DATA.date, new Date(lastModified));
-        }
+	private String getRedirectedUrl(URL url, HttpURLConnection connection) throws IOException {
+		String newLocation = connection.getHeaderField("Location");
+		if (newLocation == null) {
+			throw new IOException("missing redirection location");
+		}
+		else {
+			return new URL(url, newLocation).toString();
+		}
+	}
 
-        long expires = connection.getExpiration();
-        if (expires != 0L) {
-            metadata.add(DATA.expirationDate, new Date(expires));
-        }
+	private DataObject createDataObject(String url, DataSource source, HttpURLConnection connection,
+			RDFContainerFactory containerFactory) throws IOException {
+		// create the resulting instance
+		URI uri = new URIImpl(url);
+		RDFContainer metadata = containerFactory.getRDFContainer(uri);
 
-        return object;
-    }
+		InputStream stream = HttpClientUtil.getInputStream(connection);
+		if (!stream.markSupported()) {
+			stream = new BufferedInputStream(stream, 8192);
+		}
+
+		DataObject object = new FileDataObjectBase(uri, source, metadata, stream);
+
+		// populate the metadata
+		String characterSet = null;
+		String mimeType = null;
+
+		String contentType = connection.getContentType();
+		if (contentType != null) {
+			ContentType parsedType = new ContentType(contentType);
+			characterSet = parsedType.getCharset();
+			mimeType = parsedType.getMimeType();
+		}
+
+		if (characterSet == null) {
+			// character set defaults to ISO-8859-1 (Latin-1) for HTTP
+			characterSet = "ISO-8859-1";
+		}
+
+		metadata.add(DATA.characterSet, characterSet);
+
+		if (mimeType != null) {
+			metadata.add(DATA.mimeType, mimeType);
+		}
+
+		long contentLength = connection.getContentLength();
+		if (contentLength >= 0l) {
+			metadata.add(DATA.byteSize, contentLength);
+		}
+
+		long retrieved = connection.getDate();
+		if (retrieved != 0L) {
+			metadata.add(DATA.retrievalDate, new Date(retrieved));
+		}
+
+		long lastModified = connection.getLastModified();
+		if (lastModified != 0L) {
+			metadata.add(DATA.date, new Date(lastModified));
+		}
+
+		long expires = connection.getExpiration();
+		if (expires != 0L) {
+			metadata.add(DATA.expirationDate, new Date(expires));
+		}
+
+		return object;
+	}
 }
