6b0737096187d8493f22eaa85458a3a88942f78d
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-09-28 19:52:04 +0000

    added REDIRECTS_TO_KEY constant

--- aperture/src/java/org/semanticdesktop/aperture/accessor/AccessData.java
+++ aperture/src/java/org/semanticdesktop/aperture/accessor/AccessData.java
@@ -13,119 +13,123 @@ import java.util.Set;
  * An AccessData instance stores information about accessed resources such as last modification dates,
  * locations, etc. This primarily facilitates incremental crawling of DataSources.
  * 
- * AccessData proposes a number of keys to use when storing values, combined with a proposed value
- * encoding. This is to ensure that several DataAccessors and possibly other components can share the
- * same AccessData instance without resulting in conflicts.
+ * AccessData proposes a number of keys to use when storing values, combined with a proposed value encoding.
+ * This is to ensure that several DataAccessors and possibly other components can share the same AccessData
+ * instance without resulting in conflicts.
  */
 public interface AccessData {
 
-    /**
-     * Recommended key to store a resource's date. Recommended value encoding: time in milliseconds as a
-     * string.
-     */
-    public static final String DATE_KEY = "date";
+	/**
+	 * Recommended key to store a resource's date. Recommended value encoding: time in milliseconds as a
+	 * string.
+	 */
+	public static final String DATE_KEY = "date";
 
-    /**
-     * Recommended key to store a resource's byte size. Recommended value encoding: String-encoded long.
-     */
-    public static final String BYTE_SIZE_KEY = "byteSize";
+	/**
+	 * Recommended key to store a resource's byte size. Recommended value encoding: String-encoded long.
+	 */
+	public static final String BYTE_SIZE_KEY = "byteSize";
 
-    /**
-     * Prepares the AccessData for operation. This may for example mean reading files or opening
-     * repositories that hold the stored data.
-     */
-    public void initialize() throws IOException;
+	/**
+	 * Recommended key to store the redirected URL of an original URL. Recommended value encoding: a legal
+	 * URI.
+	 */
+	public static final String REDIRECTS_TO_KEY = "redirectsTo";
 
-    /**
-     * Informs the AccessData that processing has completed and, in case of a persistent storage, now is
-     * a good time to write or flush results. Afterwards the AccessData may be in an unusable state until
-     * 'initialize' is invoked again.
-     */
-    public void store() throws IOException;
+	/**
+	 * Prepares the AccessData for operation. This may for example mean reading files or opening repositories
+	 * that hold the stored data.
+	 */
+	public void initialize() throws IOException;
 
-    /**
-     * Clears this AccessData. This may be invoked on initialized and unititialized AccessData's. Both
-     * in-memory information as any persistent storage will be cleared. Afterwards the AccessData may be
-     * in an unusable state until 'initialize' is invoked again.
-     */
-    public void clear() throws IOException;
+	/**
+	 * Informs the AccessData that processing has completed and, in case of a persistent storage, now is a
+	 * good time to write or flush results. Afterwards the AccessData may be in an unusable state until
+	 * 'initialize' is invoked again.
+	 */
+	public void store() throws IOException;
 
-    /**
-     * Gets the number of resources for which information has been stored in this AccessData.
-     * 
-     * @return The number of registered resources.
-     */
-    public int getSize();
+	/**
+	 * Clears this AccessData. This may be invoked on initialized and unititialized AccessData's. Both
+	 * in-memory information as any persistent storage will be cleared. Afterwards the AccessData may be in an
+	 * unusable state until 'initialize' is invoked again.
+	 */
+	public void clear() throws IOException;
 
-    /**
-     * Gets the IDs of all resources for which information has been stored in this AccessData.
-     * 
-     * @return A Set of Strings.
-     */
-    public Set getStoredIDs();
+	/**
+	 * Gets the number of resources for which information has been stored in this AccessData.
+	 * 
+	 * @return The number of registered resources.
+	 */
+	public int getSize();
 
-    /**
-     * Returns whether this AccessData holds any information about the specified ID.
-     * 
-     * @return "true" when this AccessData has information about the specified ID, "false" otherwise.
-     */
-    public boolean isKnownId(String id);
+	/**
+	 * Gets the IDs of all resources for which information has been stored in this AccessData.
+	 * 
+	 * @return A Set of Strings.
+	 */
+	public Set getStoredIDs();
 
-    /**
-     * Stores information (a key-value pair) for the specified id.
-     * 
-     * @param id The resource's ID.
-     * @param key The info key.
-     * @param value The info value.
-     */
-    public void put(String id, String key, String value);
+	/**
+	 * Returns whether this AccessData holds any information about the specified ID.
+	 * 
+	 * @return "true" when this AccessData has information about the specified ID, "false" otherwise.
+	 */
+	public boolean isKnownId(String id);
 
-    /**
-     * Stores a reference relation between two resources, modeling e.d. a parent-child relationship or a
-     * link.
-     * 
-     * @param id The referring resource's ID.
-     * @param referredID The referred resource's ID.
-     */
-    public void putReferredID(String id, String referredID);
+	/**
+	 * Stores information (a key-value pair) for the specified id.
+	 * 
+	 * @param id The resource's ID.
+	 * @param key The info key.
+	 * @param value The info value.
+	 */
+	public void put(String id, String key, String value);
 
-    /**
-     * Gets specific information about the specified id.
-     * 
-     * @param id The resource's ID.
-     * @param key The info key.
-     * @return The stored info value, or null if no info has been stored for the specified id and key.
-     */
-    public String get(String id, String key);
+	/**
+	 * Stores a reference relation between two resources, modeling e.d. a parent-child relationship or a link.
+	 * 
+	 * @param id The referring resource's ID.
+	 * @param referredID The referred resource's ID.
+	 */
+	public void putReferredID(String id, String referredID);
 
-    /**
-     * Returns all referred resources of the specified resource.
-     * 
-     * @return A Set of Strings, or null when there are no referred resources registered for this
-     *         resource.
-     */
-    public Set getReferredIDs(String id);
+	/**
+	 * Gets specific information about the specified id.
+	 * 
+	 * @param id The resource's ID.
+	 * @param key The info key.
+	 * @return The stored info value, or null if no info has been stored for the specified id and key.
+	 */
+	public String get(String id, String key);
 
-    /**
-     * Removes the value for the specified id and key.
-     * 
-     * @param id A resource ID.
-     * @param key A key under which info is stored.
-     */
-    public void remove(String id, String key);
+	/**
+	 * Returns all referred resources of the specified resource.
+	 * 
+	 * @return A Set of Strings, or null when there are no referred resources registered for this resource.
+	 */
+	public Set getReferredIDs(String id);
 
-    /**
-     * Removes a reference relationship between two resources.
-     * 
-     * @param id The referring resource's ID.
-     * @param referredID The referred resource's ID.
-     */
-    public void removeReferredID(String id, String referredID);
+	/**
+	 * Removes the value for the specified id and key.
+	 * 
+	 * @param id A resource ID.
+	 * @param key A key under which info is stored.
+	 */
+	public void remove(String id, String key);
 
-    /**
-     * Removes all information about the resource with the specified ID.
-     * 
-     * @param id A resource ID.
-     */
-    public void remove(String id);
+	/**
+	 * Removes a reference relationship between two resources.
+	 * 
+	 * @param id The referring resource's ID.
+	 * @param referredID The referred resource's ID.
+	 */
+	public void removeReferredID(String id, String referredID);
+
+	/**
+	 * Removes all information about the resource with the specified ID.
+	 * 
+	 * @param id A resource ID.
+	 */
+	public void remove(String id);
 }
