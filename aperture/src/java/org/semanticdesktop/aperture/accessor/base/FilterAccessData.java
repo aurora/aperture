b6e2c8ee6aaa4401599f3322bea3456772b478f5
cfmfluit <cfmfluit@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-10-02 10:23:29 +0000

    added class

---
+++ aperture/src/java/org/semanticdesktop/aperture/accessor/base/FilterAccessData.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2006 Aduna.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.accessor.base;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.semanticdesktop.aperture.accessor.AccessData;
+
+/**
+ * Abstract superclass for AccessData implementations that wrap other AccessData instances in order to filter
+ * certain information. The abstract class FilterAccessData itself provides default methods that pass requests
+ * to the contained AccessData. Subclasses of FilterAccessData should override some of these methods and may
+ * also provide additional methods and fields.
+ */
+public abstract class FilterAccessData implements AccessData {
+
+	private AccessData accessData;
+	
+	public FilterAccessData(AccessData accessData) {
+		this.accessData = accessData;
+	}
+	
+	public AccessData getAccessData() {
+		return accessData;
+	}
+	
+	public void clear() throws IOException {
+		accessData.clear();
+	}
+
+	public String get(String id, String key) {
+		return accessData.get(id, key);
+	}
+
+	public Set getReferredIDs(String id) {
+		return accessData.getReferredIDs(id);
+	}
+
+	public int getSize() {
+		return accessData.getSize();
+	}
+
+	public Set getStoredIDs() {
+		return accessData.getStoredIDs();
+	}
+
+	public void initialize() throws IOException {
+		accessData.initialize();
+	}
+
+	public boolean isKnownId(String id) {
+		return accessData.isKnownId(id);
+	}
+
+	public void put(String id, String key, String value) {
+		accessData.put(id, key, value);
+	}
+
+	public void putReferredID(String id, String referredID) {
+		accessData.putReferredID(id, referredID);
+	}
+
+	public void remove(String id, String key) {
+		accessData.remove(id, key);
+	}
+
+	public void remove(String id) {
+		accessData.remove(id);
+	}
+
+	public void removeReferredID(String id, String referredID) {
+		accessData.removeReferredID(id, referredID);
+	}
+
+	public void removeReferredIDs(String id) {
+		accessData.removeReferredIDs(id);
+	}
+
+	public void store() throws IOException {
+		accessData.store();
+	}
+}
