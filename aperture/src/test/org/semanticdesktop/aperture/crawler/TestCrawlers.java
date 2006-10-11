42db9d04dcd0d81f36ab931b926a909f7db4c643
mylka <mylka@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-10-11 16:37:53 +0000

    Added the TestIcalCrawler test suite.

--- aperture/src/test/org/semanticdesktop/aperture/crawler/TestCrawlers.java
+++ aperture/src/test/org/semanticdesktop/aperture/crawler/TestCrawlers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 Aduna and Deutsches Forschungszentrum für Künstliche Intelligenz DFKI GmbH.
+ * Copyright (c) 2005 Aduna and Deutsches Forschungszentrum fï¿½r Kï¿½nstliche Intelligenz DFKI GmbH.
  * All rights reserved.
  * 
  * Licensed under the Academic Free License version 3.0.
@@ -10,6 +10,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.semanticdesktop.aperture.crawler.filesystem.TestFileSystemCrawler;
+import org.semanticdesktop.aperture.crawler.ical.TestIcalCrawler;
 import org.semanticdesktop.aperture.crawler.impl.TestDefaultCrawlerRegistry;
 
 /**
@@ -25,5 +26,6 @@ public class TestCrawlers extends TestSuite {
         super("crawlers");
         addTest(new TestSuite(TestFileSystemCrawler.class));
         addTest(new TestSuite(TestDefaultCrawlerRegistry.class));
+        addTest(new TestSuite(TestIcalCrawler.class));
     }
 }
