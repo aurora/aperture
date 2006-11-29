7841b02824f86beb827bd27194f8b89e0aa63a7b
gromgull <gromgull@200f20b7-5f35-0410-b7a9-abe77d974466>
2006-11-29 14:30:53 +0000

    Removed redundant PlatformUtil Updated OSUtil to use AppleUtils class directly

--- aperture/src/java/org/semanticdesktop/aperture/util/OSUtils.java
+++ aperture/src/java/org/semanticdesktop/aperture/util/OSUtils.java
@@ -8,12 +8,20 @@ package org.semanticdesktop.aperture.util;
 
 import java.io.File;
 import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.gnowsis.util.AppleUtils;
+import org.semanticdesktop.aperture.addressbook.AddressbookCrawler;
 
 /**
  * 
  * @author grimnes
  */
 public abstract class OSUtils {
+	
+	protected static final Logger log = Logger.getLogger(OSUtils.class.getName());
+	
 	public static boolean isWindows() {
 		return System.getProperty("os.name").toLowerCase().contains("win");
 	}
@@ -26,15 +34,19 @@ public abstract class OSUtils {
 		return System.getProperty("os.name").toLowerCase().contains("linux");
 	}
 	
+	/**
+	 * Checks if the given directory is a MacOSX bundle. 
+	 * This only makes sense on MaxOSX, and will always return false on other OS. 
+	 * @param f - the directory to check
+	 * @return
+	 */
 	public static boolean isMacOSXBundle(File f) {
-		Class apc;
+		if (!isMac()) return false; 
 		try {
-			apc = OSUtils.class.getClassLoader().loadClass("org.gnowsis.util.AppleUtils");
-			Method m = apc.getMethod("isBundle",new Class[] { File.class });
-			return ((Boolean)m.invoke(null,new Object[] { f })).booleanValue();
+			return AppleUtils.isBundle(f); 
 		}
-		catch (Exception e) {
-			e.printStackTrace();
+		catch (Throwable e) {
+			log.log(Level.INFO, "Could not check if directoy was bundle, assuming not. ",e);
 			return false;
 		}
 		
