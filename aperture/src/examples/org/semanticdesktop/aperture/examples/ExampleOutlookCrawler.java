330551bef7fb8a98b34ca1c5f3070d8bbd7cdc76
leo_sauermann <leo_sauermann@200f20b7-5f35-0410-b7a9-abe77d974466>
2007-07-10 08:50:33 +0000

    added an outlook example, but not implemented

---
+++ aperture/src/examples/org/semanticdesktop/aperture/examples/ExampleOutlookCrawler.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2006 Aduna and Deutsches Forschungszentrum fuer Kuenstliche Intelligenz DFKI GmbH.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.examples;
+
+
+public class ExampleOutlookCrawler {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO: use the extracted simplecrawler handler
+
+    }
+
+}
+
