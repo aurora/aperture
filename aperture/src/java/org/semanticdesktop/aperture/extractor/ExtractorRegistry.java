fc06b8ca02e36170782f1e8cf79022b5747b59d6
mylka <mylka@200f20b7-5f35-0410-b7a9-abe77d974466>
2007-12-15 16:13:04 +0000

    added the FileExtractor interfaces and the AbstractFileExtractor class to the extractor package

--- aperture/src/java/org/semanticdesktop/aperture/extractor/ExtractorRegistry.java
+++ aperture/src/java/org/semanticdesktop/aperture/extractor/ExtractorRegistry.java
@@ -24,10 +24,23 @@ public interface ExtractorRegistry {
     public void remove(ExtractorFactory factory);
 
     /**
-     * Returns all ExtractorFactories that support the specified MIME ype.
+     * Adds a file extractor factory to this registry.
+     */
+    public void add(FileExtractorFactory factory);
+
+    /**
+     * Remove a FileExtractorFactory from this registry.
+     */
+    public void remove(FileExtractorFactory factory);
+
+    /**
+     * Returns all ExtractorFactories that support the specified MIME type.
      * 
-     * @return A Set of ExtractorFactories whose getSupportedMimeTypes method results contain the
-     *         specified MIME type.
+     * @return A Set of ExtractorFactories whose getSupportedMimeTypes method results contain the specified
+     *         MIME type.
+     * @deprecated Use getExtractorFactories instead. After the introduction of FileExtractorFactories the
+     *             semantics of this method may have become unclear. It returns normal stream-based extractor
+     *             factories. The result is the same as {@link #getExtractorFactories}
      */
     public Set get(String mimeType);
 
@@ -35,6 +48,39 @@ public interface ExtractorRegistry {
      * Returns all ExtractorFactories registered in this ExtractorRegistry.
      * 
      * @return A Set of ExtractorFactory instances.
+     * @deprecated Use getAllExtractorFactories instead. After the introduction of FileExtractorFactories the
+     *             semantics of this method may have become unclear. It returns normal stream-based extractor
+     *             factories. The result is the same as {@link #getAllExtractorFactories}
      */
     public Set getAll();
+
+    /**
+     * Returns all ExtractorFactories that support the specified MIME type.
+     * 
+     * @return A Set of ExtractorFactories whose getSupportedMimeTypes method results contain the specified
+     *         MIME type.
+     */
+    public Set getExtractorFactories(String mimeType);
+
+    /**
+     * Returns all ExtractorFactories registered in this ExtractorRegistry.
+     * 
+     * @return A Set of ExtractorFactory instances.
+     */
+    public Set getAllExtractorFactories();
+    
+    /**
+     * Returns all FileExtractorFactories that support the specified MIME type.
+     * 
+     * @return A Set of ExtractorFactories whose getSupportedMimeTypes method results contain the specified
+     *         MIME type.
+     */
+    public Set getFileExtractorFactories(String mimeType);
+
+    /**
+     * Returns all FileExtractorFactories registered in this ExtractorRegistry.
+     * 
+     * @return A Set of ExtractorFactory instances.
+     */
+    public Set getAllFileExtractorFactories();
 }
