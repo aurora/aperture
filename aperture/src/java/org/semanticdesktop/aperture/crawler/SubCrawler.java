451c630717a780a3eec234ca1685cb80cafcad68
mylka <mylka@200f20b7-5f35-0410-b7a9-abe77d974466>
2008-02-07 18:12:50 +0000

    The first version of the SubCrawler API

--- aperture/src/java/org/semanticdesktop/aperture/crawler/SubCrawler.java
+++ aperture/src/java/org/semanticdesktop/aperture/crawler/SubCrawler.java
@@ -6,77 +6,52 @@
  */
 package org.semanticdesktop.aperture.crawler;
 
-import org.ontoware.rdf2go.model.node.URI;
+import java.io.InputStream;
+
 import org.semanticdesktop.aperture.accessor.AccessData;
-import org.semanticdesktop.aperture.accessor.DataAccessorRegistry;
-import org.semanticdesktop.aperture.datasource.DataSource;
+import org.semanticdesktop.aperture.rdf.RDFContainer;
 
 /**
- * A SubCrawler accesses a FileDataObject and produces a stream of other DataObjects representing the
+ * A SubCrawler accesses an InputStream and produces a stream of other DataObjects representing the
  * resources found "inside".
  * 
  * <p>
  * An AccessData instance can optionally be specified to a SubCrawler, allowing it to perform incremental
- * crawling, i.e. to scan and report the differences in the FileDataObject since the last crawl.
+ * crawling, i.e. to scan and report the differences in the stream since the last crawl.
  */
 public interface SubCrawler {
 
     /**
-     * Sets the AccessData instance to be used.
+     * Starts crawling the given stream and to report the encountered DataObjects to the given CrawlerHandler.
+     * If an AccessData instance is passed, it is used to check if the data objects are to be reported as new,
+     * modified, unmodified or deleted.
      * 
-     * @param accessData The AccessData instance to use, or 'null' when no AccessData is to be used.
+     * @param stream the stream to be crawled.
+     * @param accessData the AccessData used to determine if the encountered objects are to be returned as
+     *            new, modified, unmodified or deleted. Information about new or modified objects is stored
+     *            within for use in future crawls. This parameter may be null if this functionality is not
+     *            desired, in which case all DataObjects will be reported as new.
+     * @param handler The crawler handler that is to receive the notifications from the SubCrawler
+     * @param result The 'parent' RDFContainer, that will contain the metadata about the top-level entity in
+     *            the stream. A SubCrawler may (in some cases) limit itself to augmenting the metadata in this
+     *            RDFContainer without delivering any additional DataObjects.
      */
-    public void setAccessData(AccessData accessData);
-
+    public void subCrawl(InputStream stream, AccessData accessData, CrawlerHandler handler,
+            RDFContainer result);
+    
     /**
-     * Returns the AccessData used by this Crawler.
+     * Clears the information the SubCrawler had about the state of the stream from the given AccessData
+     * instance. The given handler instance receives notifications about the clearing process. See the
+     * {@link Crawler#clear()} documentation for more details.
      * 
-     * @return The AccessData used by this Crawler, or 'null' when no AccessData is used.
+     * @param data the accessData that is to be cleared
+     * @param handler the CrawlerHandler that is to receive notifications
      */
-    public AccessData getAccessData();
-
+    public void clearSubCrawledResults(AccessData data, CrawlerHandler handler);
+    
     /**
-     * Starts crawling the domain defined in the DataSource of this Crawler. If this is a subsequent run of
-     * this method, it will only report the differences with the previous run, unless the previous scan
-     * results have been cleared. Any CrawlerListeners registered on this Crawler will get notified about the
-     * crawling progress.
-     */
-    public void crawl();
-
-    /**
-     * Clears all stored crawl results. Any listeners registered with this data source will be notified of the
-     * removal of the individual crawl results. The next call to crawl() will again report all DataObjects in
-     * the configured domain.
-     */
-    public void clear();
-
-    /**
-     * Stops a running crawl or clear operation as fast as possible. This method may return before the
+     * Stops a running crawl or a clear operation as fast as possible. This method may return before the
      * crawling has actually stopped.
      */
-    public void stop();
-
-    /**
-     * Gets the CrawlReport of the last performed crawl, or the current crawl when it is in progress. Returns
-     * null when no crawl has been performed in this application's session yet and there is no report
-     * available from the previous session.
-     * 
-     * @return The CrawlReport of the last run, or null when this is not available.
-     */
-    public CrawlReport getCrawlReport();
-
-    /**
-     * Sets the CrawlerHandler to which this Crawler should report any scanned or cleared resources and from
-     * which it obtains RDFContainer.
-     * 
-     * @param handler The CrawlerHandler to register.
-     */
-    public void setCrawlerHandler(CrawlerHandler handler);
-
-    /**
-     * Returns the currently registered CrawlerHandler.
-     * 
-     * @return The current CrawlerHandler.
-     */
-    public CrawlerHandler getCrawlerHandler();
+    public void stopSubCrawler();
 }
---
+++ aperture/src/java/org/semanticdesktop/aperture/crawler/SubCrawlerException.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2005 Aduna and Deutsches Forschungszentrum fuer Kuenstliche Intelligenz DFKI GmbH.
+ * All rights reserved.
+ * 
+ * Licensed under the Academic Free License version 3.0.
+ */
+package org.semanticdesktop.aperture.crawler;
+
+/**
+ * Thrown to indicate that an error occurred while extracting information from an InputStream by an
+ * Extractor. A typical use case of this exception is for reporting parse errors.
+ */
+public class SubCrawlerException extends Exception {
+
+    /** the serial version uid */
+    private static final long serialVersionUID = 5850947385737157681L;
+
+    /**
+     * Constructs an ExtractorException with no detail message.
+     */
+    public SubCrawlerException() {
+        super();
+    }
+
+    /**
+     * Constructs a SubCrawlerException with the specified detail message.
+     * 
+     * @param msg The detail message.
+     */
+    public SubCrawlerException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs an SubCrawlerException with the specified detail message and cause.
+     * 
+     * Note that the detail message associated with the cause is not automatically incorporated in this
+     * exception's detail message.
+     * 
+     * @param msg The detail message.
+     * @param source The cause, which is saved for later retrieval by the Throwable.getCause() method. A
+     *            null value is permitted, and indicates that the cause is nonexistent or unknown.
+     */
+    public SubCrawlerException(String msg, Throwable source) {
+        super(msg, source);
+    }
+
+    /**
+     * Constructs a SubCrawlerException with the specified cause and a detail message of <tt>(cause==null ?
+     * null : cause.toString())</tt> (which typically contains the class and detail message of cause).
+     * This constructor is useful for exceptions that are little more than wrappers for other throwables.
+     * 
+     * @param source The cause, which is saved for later retrieval by the Throwable.getCause() method. A
+     *            null value is permitted, and indicates that the cause is nonexistent or unknown.
+     */
+    public SubCrawlerException(Throwable source) {
+        super(source);
+    }
+}
