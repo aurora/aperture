edbb7f730a2b0a4d8c904b5d97d3a818afbce83f
mylka <mylka@200f20b7-5f35-0410-b7a9-abe77d974466>
2008-04-28 20:28:17 +0000

    fixed an issue in the html link extractor reported by Grant Ingersol. <a href tags inside java scripts are ignored

--- aperture/src/test/org/semanticdesktop/aperture/hypertext/linkextractor/html/HtmlLinkExtractorTest.java
+++ aperture/src/test/org/semanticdesktop/aperture/hypertext/linkextractor/html/HtmlLinkExtractorTest.java
@@ -11,6 +11,9 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 
+import org.ontoware.rdf2go.RDF2Go;
+import org.ontoware.rdf2go.model.Model;
+import org.ontoware.rdf2go.model.node.URI;
 import org.semanticdesktop.aperture.ApertureTestBase;
 import org.semanticdesktop.aperture.hypertext.linkextractor.LinkExtractor;
 import org.semanticdesktop.aperture.util.ResourceUtil;
@@ -19,32 +22,41 @@ public class HtmlLinkExtractorTest extends ApertureTestBase {
 
     private InputStream stream;
 
-    public void setUp() {
-        stream = ResourceUtil.getInputStream(
-            "org/semanticdesktop/aperture/hypertext/linkextractor/html/test.html",
-            HtmlLinkExtractorTest.class);
-    }
+    private static final String TEST_RESOURCE = "org/semanticdesktop/aperture/hypertext/linkextractor/html/test.html"; 
 
+    private static final String CONDENAST_RESOURCE = "org/semanticdesktop/aperture/docs/condenast.html";
+    
     public void tearDown() throws IOException {
         stream.close();
     }
 
     public void testNavigationLinks() throws IOException {
-        List links = getLinks(Boolean.FALSE);
+        List links = getLinks(Boolean.FALSE, TEST_RESOURCE);
         assertEquals(2, links.size());
         assertTrue(links.contains("http://example.com/absolute/link"));
         assertTrue(links.contains("http://example.com/relative/link"));
     }
 
     public void testAllLinks() throws IOException {
-        List links = getLinks(Boolean.TRUE);
+        List links = getLinks(Boolean.TRUE, TEST_RESOURCE);
         assertEquals(3, links.size());
         assertTrue(links.contains("http://example.com/absolute/link"));
         assertTrue(links.contains("http://example.com/relative/link"));
         assertTrue(links.contains("http://example.com/image"));
     }
+    
+    public void testCondeNast() throws IOException {
+        List links = getLinks(Boolean.FALSE, CONDENAST_RESOURCE);
+        Model model = RDF2Go.getModelFactory().createModel();
+        model.open();
+        // every link should be a valid uri
+        for (Object obj : links) {
+            URI uri = model.createURI(obj.toString());
+        }
+    }
 
-    private List getLinks(Boolean includeEmbeddedResources) throws IOException {
+    private List getLinks(Boolean includeEmbeddedResources, String resource) throws IOException {
+        stream = ResourceUtil.getInputStream(resource, HtmlLinkExtractorTest.class);
         HtmlLinkExtractor extractor = new HtmlLinkExtractor();
         HashMap params = new HashMap();
         params.put(LinkExtractor.INCLUDE_EMBEDDED_RESOURCES_KEY, includeEmbeddedResources);
