b8a8668ec5394af5acf4a5d00c911954cb163768
mylka <mylka@200f20b7-5f35-0410-b7a9-abe77d974466>
2009-02-04 01:32:38 +0000

    2408096 - applied herko's patch to the DeliciousCrawler and removed all traces of the DFKIUtils2.jar from the project

--- aperture/src/java/org/semanticdesktop/aperture/websites/delicious/DeliciousCrawler.java
+++ aperture/src/java/org/semanticdesktop/aperture/websites/delicious/DeliciousCrawler.java
@@ -6,6 +6,9 @@
  */
 package org.semanticdesktop.aperture.websites.delicious;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.Date;
 import java.util.HashMap;
@@ -13,6 +16,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.Vector;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpClient;
@@ -43,194 +50,217 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import de.dfki.util.xml.XML;
-import de.dfki.util.xml.XML.DOM;
-
 /**
  * A del.icio.us crawlers
  */
 public class DeliciousCrawler extends AbstractTagCrawler {
 
-    private Logger log=LoggerFactory.getLogger(DeliciousCrawler.class);
-    
+    private Logger log = LoggerFactory.getLogger(DeliciousCrawler.class);
+
     /** URL to the tags API */
-	public static final String TAGS_API="del.icio.us/api/tags/get";
-	/** URL to the posts API */
-	public static final String POSTS_API="del.icio.us/api/posts/all";
-	
-	private WebAccessData wad= null;
+    public static final String TAGS_API = "del.icio.us/api/tags/get";
+
+    /** URL to the posts API */
+    public static final String POSTS_API = "del.icio.us/api/posts/all";
+
+    private WebAccessData wad = null;
+
     private LinkedList<CrawlJob> jobsQueue;
+
     private HashMap<String, CrawlJob> jobsMap;
-	
-	/**
-	 * @param dataSource
-	 */
-	public DeliciousCrawler(DataSource dataSource) {
-		super();
-		setDataSource(dataSource);
-		
-		  // initialize variables
+
+    /**
+     * @param dataSource
+     */
+    public DeliciousCrawler(DataSource dataSource) {
+        super();
+        setDataSource(dataSource);
+
+        // initialize variables
         jobsQueue = new LinkedList<CrawlJob>();
         jobsMap = new HashMap<String, CrawlJob>(1024);
-       
-        
-	}
 
-	/* (non-Javadoc)
-	 * @see org.semanticdesktop.aperture.websites.TagCrawler#crawlTags(java.lang.String)
-	 */
-	@Override
-	protected List<String> crawlTags(String username, String password) throws java.lang.Exception {
-		
-		//String url="http://"+username+":"+password+"@"+TAGS_API;
-		String url="http://"+TAGS_API;
-		System.out.println(url);
-		List<String> res=new Vector<String>();
+    }
 
-		Credentials c=new UsernamePasswordCredentials(username,password);
-		HttpClient hc=new HttpClient();
-		hc.getState().setCredentials(AuthScope.ANY,c);
-		
-		HttpMethod method=new GetMethod(url);
-		method.setDoAuthentication(true);
-		
-		int http_code=hc.executeMethod(method);
-		if (http_code!=HttpURLConnection.HTTP_OK) {
-			throw new Exception("HTTP Method did not return OK for url '"+url+"' code: "+http_code);
-		}
-		
-		DOM dom = XML.dom();
-		Document d = dom.readFromInputStream(method.getResponseBodyAsStream());
-		NodeList l = d.getElementsByTagName("tag");
-		int i=0;
-		while(l.item(i)!=null) {
-			Element e=(Element)l.item(i);
-			res.add("http://del.icio.us/"+username+"/"+e.getAttribute("tag"));
-			i++;
-		}
-		
-		return res;
-		
-	}
-	/**
-	 * Crawl POSTS from del.icio.us
-	 * @throws Exception 
-	 * @throws SAXException 
-	 */
-	@Override
-	protected void crawlTheRest(String username, String pass) throws SAXException, Exception{
-	  
-        String url="http://"+POSTS_API;
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.semanticdesktop.aperture.websites.TagCrawler#crawlTags(java.lang.String)
+     */
+    @Override
+    protected List<String> crawlTags(String username, String password) throws java.lang.Exception {
+
+        // String url="http://"+username+":"+password+"@"+TAGS_API;
+        String url = "http://" + TAGS_API;
         System.out.println(url);
-        DataSource localSource = getDataSource();
-        
-        Credentials c=new UsernamePasswordCredentials(username,pass);
-        HttpClient hc=new HttpClient();
-        hc.getState().setCredentials(AuthScope.ANY,c);
-        
-        HttpMethod method=new GetMethod(url);
+        List<String> res = new Vector<String>();
+
+        Credentials c = new UsernamePasswordCredentials(username, password);
+        HttpClient hc = new HttpClient();
+        hc.getState().setCredentials(AuthScope.ANY, c);
+
+        HttpMethod method = new GetMethod(url);
         method.setDoAuthentication(true);
-        
-        int http_code=hc.executeMethod(method);
-        if (http_code!=HttpURLConnection.HTTP_OK) {
-            throw new Exception("HTTP Method did not return OK for url '"+url+"' code: "+http_code);
+
+        int http_code = hc.executeMethod(method);
+        if (http_code != HttpURLConnection.HTTP_OK) {
+            throw new Exception("HTTP Method did not return OK for url '" + url + "' code: " + http_code);
         }
-        
-        DOM dom = XML.dom();
-        Document d = dom.readFromInputStream(method.getResponseBodyAsStream());
-        
-        
-        NodeList l = d.getElementsByTagName("post");
-        
-        int i=0;
-        while(l.item(i)!=null) {
-            Element e=(Element)l.item(i);
-            // posts.add("http://del.icio.us/"+username+"/"+e.getAttribute("href"));
-            String postHref = e.getAttribute("href");
-            
-            URI postURI = new URIImpl(postHref);
-            
-            // add website
-            if(accessData.isKnownId(postHref)){
-                reportUnmodifiedDataObject(postHref);
-            }else{
-                accessData.put(postHref, AccessData.DATE_KEY, Long.toString(new Date().getTime()));
-                RDFContainer rdf;
-              
-                
-                DataAccessor accessor = getDataAccessor(postHref);
-                RDFContainerFactory containerFactory = getRDFContainerFactory(postHref);
-                //wad = new WebAccessData(accessData);
-                DataObject o;
-                try { 
-                    o = accessor.getDataObject(postHref, source, null, containerFactory);
-                    rdf=o.getMetadata();
-                } catch (Throwable ex) { 
-                    // "sub-crawling" the posted URI failed, 
-                    // just make up an empty data-object
-                    log.debug("Could not crawl web-site, continuing. ", ex);
-                    rdf=getRDFContainerFactory(postHref).getRDFContainer(postURI);
-                    o=new DataObjectBase(postURI, source, rdf);
-                }
-                
-                rdf.add(RDF.type, NFO.Website);
-              
-                //add hasTag
-               
-                String tags = e.getAttribute("tag");
-                for(String tag : tags.split(" ")){
-                    rdf.add(NAO.hasTag,new URIImpl("http://del.icio.us/"+username+"/"+tag));
-                };
-                reportNewDataObject(o);
-            }
-            
+
+        Document d = getDocument(method.getResponseBodyAsStream());
+        NodeList l = d.getElementsByTagName("tag");
+        int i = 0;
+        while (l.item(i) != null) {
+            Element e = (Element) l.item(i);
+            res.add("http://del.icio.us/" + username + "/" + e.getAttribute("tag"));
             i++;
         }
- 
-            
-	}
-	
-	 private DataAccessor getDataAccessor(String url) {
-	        // determine the scheme
-	        int index = url.indexOf(':');
-	        if (index <= 0) {
-	            return null;
-	        }
-	        String scheme = url.substring(0, index);
 
-	        // fetch a DataAccessor for this scheme
-	        Set factories = accessorRegistry.get(scheme);
-	        if (factories.isEmpty()) {
-	            return null;
-	        }
-	        DataAccessorFactory factory = (DataAccessorFactory) factories.iterator().next();
-	        return factory.get();
-	    }
-	 
-	 private class WebAccessData extends FilterAccessData {
+        return res;
 
-	        public WebAccessData(AccessData accessData) {
-	            super(accessData);
-	        }
+    }
 
-	        public void put(String id, String key, String value) {
-	            // Make sure the original URL is not accessed anymore. We need to do this using such a complicated
-	            // approach (using a wrapped AccessData instance) because there may be several redirection steps
-	            // between the URL passed to the DataAccessor and the URL of the final DataObject.
-	            if (REDIRECTS_TO_KEY.equals(key)) {
-	                // do this with the id rather than the value: processingQueue depends on this in order to be
-	                // able to do a crawledUrls.contains on the last URL in the redirection chain
-	                //crawledUrls.add(id); // this is obviously not needed, if we use AccessData, then crawledUrls is null
-	                //deprecatedUrls.remove(id);
-	                touch(id);
+    /**
+     * Crawl POSTS from del.icio.us
+     * 
+     * @throws Exception
+     * @throws SAXException
+     */
+    @Override
+    protected void crawlTheRest(String username, String pass) throws SAXException, Exception {
 
-	                CrawlJob job = jobsMap.remove(id);
-	                if (job != null) {
-	                    jobsQueue.remove(job);
-	                }
-	            }
+        String url = "http://" + POSTS_API;
+        System.out.println(url);
+        DataSource localSource = getDataSource();
 
-	            super.put(id, key, value);
-	        }
-	    }
+        Credentials c = new UsernamePasswordCredentials(username, pass);
+        HttpClient hc = new HttpClient();
+        hc.getState().setCredentials(AuthScope.ANY, c);
+
+        HttpMethod method = new GetMethod(url);
+        method.setDoAuthentication(true);
+
+        int http_code = hc.executeMethod(method);
+        if (http_code != HttpURLConnection.HTTP_OK) {
+            throw new Exception("HTTP Method did not return OK for url '" + url + "' code: " + http_code);
+        }
+
+        Document d = getDocument(method.getResponseBodyAsStream());
+        NodeList l = d.getElementsByTagName("post");
+        int i = 0;
+        while (l.item(i) != null) {
+            Element e = (Element) l.item(i);
+            // posts.add("http://del.icio.us/"+username+"/"+e.getAttribute("href"));
+            String postHref = e.getAttribute("href");
+
+            URI postURI = new URIImpl(postHref);
+
+            // add website
+            if (accessData.isKnownId(postHref)) {
+                reportUnmodifiedDataObject(postHref);
+            }
+            else {
+                accessData.put(postHref, AccessData.DATE_KEY, Long.toString(new Date().getTime()));
+                RDFContainer rdf;
+
+                DataAccessor accessor = getDataAccessor(postHref);
+                RDFContainerFactory containerFactory = getRDFContainerFactory(postHref);
+                // wad = new WebAccessData(accessData);
+                DataObject o;
+                try {
+                    o = accessor.getDataObject(postHref, source, null, containerFactory);
+                    rdf = o.getMetadata();
+                }
+                catch (Throwable ex) {
+                    // "sub-crawling" the posted URI failed,
+                    // just make up an empty data-object
+                    log.debug("Could not crawl web-site, continuing. ", ex);
+                    rdf = getRDFContainerFactory(postHref).getRDFContainer(postURI);
+                    o = new DataObjectBase(postURI, source, rdf);
+                }
+
+                rdf.add(RDF.type, NFO.Website);
+
+                // add hasTag
+
+                String tags = e.getAttribute("tag");
+                for (String tag : tags.split(" ")) {
+                    rdf.add(NAO.hasTag, new URIImpl("http://del.icio.us/" + username + "/" + tag));
+                }
+                ;
+                reportNewDataObject(o);
+            }
+
+            i++;
+        }
+
+    }
+
+    private Document getDocument(InputStream in) throws IOException, ParserConfigurationException,
+            SAXException {
+        Document result = null;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(false);
+        factory.setNamespaceAware(false);
+        factory.setSchema(null);
+
+        try {
+            in = new BufferedInputStream(in);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            result = builder.parse(in);
+        }
+        finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+
+        return result;
+    }
+
+    private DataAccessor getDataAccessor(String url) {
+        // determine the scheme
+        int index = url.indexOf(':');
+        if (index <= 0) {
+            return null;
+        }
+        String scheme = url.substring(0, index);
+
+        // fetch a DataAccessor for this scheme
+        Set factories = accessorRegistry.get(scheme);
+        if (factories.isEmpty()) {
+            return null;
+        }
+        DataAccessorFactory factory = (DataAccessorFactory) factories.iterator().next();
+        return factory.get();
+    }
+
+    private class WebAccessData extends FilterAccessData {
+
+        public WebAccessData(AccessData accessData) {
+            super(accessData);
+        }
+
+        public void put(String id, String key, String value) {
+            // Make sure the original URL is not accessed anymore. We need to do this using such a complicated
+            // approach (using a wrapped AccessData instance) because there may be several redirection steps
+            // between the URL passed to the DataAccessor and the URL of the final DataObject.
+            if (REDIRECTS_TO_KEY.equals(key)) {
+                // do this with the id rather than the value: processingQueue depends on this in order to be
+                // able to do a crawledUrls.contains on the last URL in the redirection chain
+                // crawledUrls.add(id); // this is obviously not needed, if we use AccessData, then
+                // crawledUrls is null
+                // deprecatedUrls.remove(id);
+                touch(id);
+
+                CrawlJob job = jobsMap.remove(id);
+                if (job != null) {
+                    jobsQueue.remove(job);
+                }
+            }
+
+            super.put(id, key, value);
+        }
+    }
 }
